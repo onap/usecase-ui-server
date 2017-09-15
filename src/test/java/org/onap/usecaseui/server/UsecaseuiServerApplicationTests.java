ae610ac93dc1c24577f04021c7211179d92bb5e7
shentao <shentao@chinamobile.com>
2017-09-15 15:08:27 +0800

    Upload usecase-ui-server structure code
    Change-Id: I1c9d7cfc4814308d745cd79db12594a79b815999
    Issue-Id: USECASEUI-7
    Signed-off-by: shentao <shentao@chinamobile.com>

---
+++ src/test/java/org/onap/usecaseui/server/UsecaseuiServerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.onap.usecaseui.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsecaseuiServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
