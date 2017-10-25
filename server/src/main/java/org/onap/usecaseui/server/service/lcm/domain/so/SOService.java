09829f99c8d2abb2b6f461870d0195e7c6465eb7
Luji7 <lu.ji3@zte.com.cn>
2017-10-25 11:55:17 +0800

    add so auth in request header.
    Change-Id: Icedd39ba471792905e4a4b1a6af7d94586a1f8c0
    Issue-id: USECASEUI-53
    Signed-off-by: Luji7 <lu.ji3@zte.com.cn>

--- server/src/main/java/org/onap/usecaseui/server/service/lcm/domain/so/SOService.java
+++ server/src/main/java/org/onap/usecaseui/server/service/lcm/domain/so/SOService.java
@@ -24,12 +24,24 @@ import retrofit2.http.*;
 
 public interface SOService {
 
+    @Headers({
+            "Authorization: Basic SW5mcmFQb3J0YWxDbGllbnQ6cGFzc3dvcmQxJA==",
+            "Accept: application/json"
+    })
     @POST("/api/e2eServiceInstances/v3")
     Call<ServiceOperation> instantiateService(@Body RequestBody body);
 
+    @Headers({
+            "Authorization: Basic SW5mcmFQb3J0YWxDbGllbnQ6cGFzc3dvcmQxJA==",
+            "Accept: application/json"
+    })
     @GET("/api/e2eServiceInstances/v3/{serviceId}/operations/{operationId}")
     Call<OperationProgressInformation> queryOperationProgress(@Path("serviceId") String serviceId, @Path("operationId") String operationId);
 
+    @Headers({
+            "Authorization: Basic SW5mcmFQb3J0YWxDbGllbnQ6cGFzc3dvcmQxJA==",
+            "Accept: application/json"
+    })
     @DELETE("/api/e2eServiceInstances/v3/{serviceId}")
     Call<DeleteOperationRsp> terminateService(@Path("serviceId") String serviceId);
 }
