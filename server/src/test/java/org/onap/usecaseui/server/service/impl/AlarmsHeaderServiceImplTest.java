98beaf7ce00a9a0abf22485d944fc5c9490132e4
zhangab <zhanganbing@chinamobile.com>
2018-03-27 11:06:25 +0800

    improve sonar coverage for uui-server
    Change-Id: I999cf65b9beae34a6775377294643b32352d92fd
    Issue-ID: USECASEUI-103
    Signed-off-by: zhangab <zhanganbing@chinamobile.com>

--- server/src/test/java/org/onap/usecaseui/server/service/impl/AlarmsHeaderServiceImplTest.java
+++ server/src/test/java/org/onap/usecaseui/server/service/impl/AlarmsHeaderServiceImplTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2017 CMCC, Inc. and others. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.onap.usecaseui.server.service.impl; 
 
 import org.junit.Test; 
@@ -39,7 +24,11 @@ import java.util.Date;
 * @since <pre>8, 2018</pre>
 * @version 1.0 
 */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = UsecaseuiServerApplication.class)
+@WebAppConfiguration
 public class AlarmsHeaderServiceImplTest {
+    @Autowired
     private AlarmsHeaderServiceImpl alarmsHeaderServiceImpl;
 
 @Before
@@ -57,6 +46,7 @@ public void after() throws Exception {
 */ 
 @Test
 public void testSaveAlarmsHeader() throws Exception { 
+//TODO: Test goes here...
     AlarmsHeader a = new AlarmsHeader();
     a.setEventName("a");
     a.setStatus("1");
@@ -86,4 +76,6 @@ public void testSaveAlarmsHeader() throws Exception {
     a.setVersion("va2");
     System.out.println(alarmsHeaderServiceImpl.saveAlarmsHeader(a));
 } 
+
+
 } 
