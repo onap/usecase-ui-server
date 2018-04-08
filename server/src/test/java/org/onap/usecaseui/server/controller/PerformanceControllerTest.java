ebca02b52e26276629d9734f455a737150520b05
zhangab <zhanganbing@chinamobile.com>
2018-04-08 14:13:15 +0800

    improve sonar coverage for uui-server
    Change-Id: I90bf3167abe3d6a383c0147597b1c69c1e319920
    Issue-ID: USECASEUI-103
    Signed-off-by: zhangab <zhanganbing@chinamobile.com>

--- server/src/test/java/org/onap/usecaseui/server/controller/PerformanceControllerTest.java
+++ server/src/test/java/org/onap/usecaseui/server/controller/PerformanceControllerTest.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (C) 2017 CMCC, Inc. and others. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,6 +25,11 @@ import org.onap.usecaseui.server.service.PerformanceInformationService;
 
 import javax.servlet.http.HttpServletResponse;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.mockito.Mockito.*;
 
 /** 
@@ -35,6 +40,7 @@ import static org.mockito.Mockito.*;
 * @version 1.0 
 */ 
 public class PerformanceControllerTest {
+    PerformanceController controller_s;
     PerformanceController controller = new PerformanceController();
     PerformanceHeaderService service;
     PerformanceInformationService perservece;
@@ -43,8 +49,10 @@ public class PerformanceControllerTest {
     @Before
 public void before() throws Exception {
     service = mock(PerformanceHeaderService.class);
+        perservece = mock(PerformanceInformationService.class);
     controller.setPerformanceHeaderService(service);
     controller.setPerformanceInformationService(perservece);
+        controller_s = new PerformanceController();
 } 
 
 @After
@@ -75,7 +83,49 @@ public void testGetPerformanceData() throws Exception {
 
 }
 
-/** 
+
+
+    @Test
+    public void testGetPerformanceData_2() throws Exception {
+//TODO: Test goes here...
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        int currentPage = 1;
+        int pageSize=12;
+        String sourceId=null;
+        String  sourceName=null;
+        String  priority=null;
+        String  startTime=null;
+        String  endTime=null;
+        PerformanceHeader header = new PerformanceHeader();
+
+
+        controller.getPerformanceData(response,currentPage,pageSize,sourceId,sourceName,priority,startTime,endTime);
+        verify(service,times(1)).queryPerformanceHeader(header,currentPage,pageSize);
+
+    }
+
+
+
+    @Test
+    public void testGetPerformanceData_3() throws Exception {
+//TODO: Test goes here...
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        int currentPage = 1;
+        int pageSize=12;
+        String sourceId="1101ZTHX1MNE1NK7E0";
+        String  sourceName="1101ZTHX1MNE1NK7E0";
+        String  priority="Normal";
+        String  startTime="2017-11-15";
+        String  endTime="2017-11-15";
+        PerformanceHeader header = new PerformanceHeader();
+
+
+        controller.getPerformanceData(response,currentPage,pageSize,sourceId,sourceName,priority,startTime,endTime);
+        verify(service,times(1)).queryPerformanceHeader(header,currentPage,pageSize);
+
+    }
+
+    /**
 * 
 * Method: generateDiagram(@RequestParam String sourceId, @RequestParam String startTime, @RequestParam String endTime, @RequestParam String nameParent, @RequestParam String format) 
 * 
@@ -90,9 +140,22 @@ public void testGenerateDiagram() throws Exception {
     String nameParent="";
     String format="";
     controller.generateDiagram(sourceId,startTime,endTime,nameParent,format);
-} 
+}
 
-/** 
+
+    @Test
+    public void testGenerateDiagram_a() throws Exception {
+//TODO: Test goes here...
+
+        String sourceId="";
+        String startTime="2017-10-15";
+        String endTime="2017-11-15";
+        String nameParent="";
+        String format="";
+        controller.generateDiagram(sourceId,startTime,endTime,nameParent,"0000");
+    }
+
+    /**
 * 
 * Method: getSourceIds() 
 * 
@@ -112,10 +175,10 @@ public void testGetSourceIds() throws Exception {
 @Test
 public void testGetNames() throws Exception { 
 //TODO: Test goes here...
-Object sourceId="1101ZTHX1MNE1NK7E0";
+Object sourceId="1101ZTHX1MMEGJM1W1";
     controller.getNames(sourceId);
-    //verify(perservece,times(1)).queryDateBetween(sourceId.toString(),null,null,null);
-} 
+    verify(perservece,times(1)).queryDateBetween(sourceId.toString(),null,null,null);
+}
 
 
 /** 
@@ -126,16 +189,35 @@ Object sourceId="1101ZTHX1MNE1NK7E0";
 @Test
 public void testDateProcess() throws Exception { 
 //TODO: Test goes here... 
-/*
+    PerformanceController per = new PerformanceController();
+    String sourceId="1101ZTHX1MMEGJM1W1";
+    String name="HO.SuccOutInterMme";
+    String time_star="2017-11-15 06:30:00";
+    //String time_itera="2017-11-15 06:31:00";
+    String time_end="2017-12-15 06:30:00";
+    SimpleDateFormat format =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date date_star = format.parse(time_star);
+    //Date date_itera = format.parse(time_itera);
+    Date date_end = format.parse(time_end);
+
+    //System.out.println(date.getTime());  //将时间转化为毫秒数例如: 1473297300000
+
+    long startTimeL =date_star.getTime();
+    long endTimeL = date_end.getTime();
+    //long timeIteraPlusVal =date_itera.getTime();
+    long timeIteraPlusVal =2592000000L;
+    long keyVal=1l;
+    long keyValIteraVal=1l;
+    String keyUnit="month";
 try {
-   Method method = PerformanceController.getClass().getMethod("dateProcess", String.class, String.class, long.class, long.class, long.class, long.class, long.class, String.class);
+   Method method = per.getClass().getDeclaredMethod("dateProcess", String.class, String.class, long.class, long.class, long.class, long.class, long.class, String.class);
    method.setAccessible(true);
-   method.invoke(<Object>, <Parameters>);
+   method.invoke(per,sourceId,name, startTimeL,endTimeL,timeIteraPlusVal,keyVal,keyValIteraVal,keyUnit);
 } catch(NoSuchMethodException e) {
 } catch(IllegalAccessException e) {
 } catch(InvocationTargetException e) {
 }
-*/
+
 } 
 
 /** 
@@ -144,18 +226,206 @@ try {
 * 
 */ 
 @Test
-public void testDiagramDate() throws Exception { 
-//TODO: Test goes here... 
-/* 
+public void testDiagramDate_year() throws Exception {
+//TODO: Test goes here...
+    PerformanceController per = new PerformanceController();
+    String sourceId="1101ZTHX1MMEGJM1W1";
+    String name="DNS.AttDnsQuery";
+    String startTime="2017-11-15 06:30:00";
+    String endTime="2017-11-16 06:30:00";
+    String format="year";
 try { 
-   Method method = PerformanceController.getClass().getMethod("diagramDate", String.class, String.class, String.class, String.class, String.class); 
+   Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
    method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
+   method.invoke(per, sourceId,name,startTime,endTime,format);
 } catch(NoSuchMethodException e) { 
 } catch(IllegalAccessException e) { 
 } catch(InvocationTargetException e) { 
 } 
-*/ 
-} 
+
+}
+
+
+
+
+    @Test
+    public void testDiagramDate_month() throws Exception {
+//TODO: Test goes here...
+        PerformanceController per = new PerformanceController();
+        String sourceId="1101ZTHX1MMEGJM1W1";
+        String name="DNS.AttDnsQuery";
+        String startTime="2017-11-15 06:30:00";
+        String endTime="2017-11-16 06:30:00";
+        String format="month";
+        try {
+            Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
+            method.setAccessible(true);
+            method.invoke(per, sourceId,name,startTime,endTime,format);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+
+    }
+
+
+
+
+    @Test
+    public void testDiagramDate_day() throws Exception {
+//TODO: Test goes here...
+        PerformanceController per = new PerformanceController();
+        String sourceId="1101ZTHX1E6M2QI4FZVTOXU";
+        String name="Dn";
+        String startTime="2017-11-15 06:30:00";
+        String endTime="2017-09-16 06:30:00";
+        String format="day";
+        try {
+            Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
+            method.setAccessible(true);
+            method.invoke(per, sourceId,name,startTime,endTime,format);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+
+    }
+
+
+
+
+    @Test
+    public void testDiagramDate_hour() throws Exception {
+//TODO: Test goes here...
+        PerformanceController per = new PerformanceController();
+        String sourceId="1101ZTHX1E6M2QI4FZVTOXU";
+        String name="rmUID";
+        String startTime="2017-11-15 06:10:00";
+        String endTime="2017-11-17 07:40:20";
+        String format="hour";
+        try {
+            Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
+            method.setAccessible(true);
+            method.invoke(per, sourceId,name,startTime,endTime,format);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+
+    }
+
+
+
+
+    @Test
+    public void testDiagramDate_minute() throws Exception {
+//TODO: Test goes here...
+        PerformanceController per = new PerformanceController();
+        String sourceId="1101ZTHX1MMEGJM1W1";
+        String name="HO.AttOutInterMme";
+        String startTime="2017-11-15 06:30:00";
+        String endTime="2017-11-16 08:20:15";
+        String format="minute";
+        try {
+            Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
+            method.setAccessible(true);
+            method.invoke(per, sourceId,name,startTime,endTime,format);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+
+    }
+
+
+
+
+    @Test
+    public void testDiagramDate_auto_hours() throws Exception {
+//TODO: Test goes here...
+        PerformanceController per = new PerformanceController();
+        String sourceId="1101ZTHX1MMEGJM1W1";
+        String name="IRATHO.AttIncFromGeran";
+        String startTime="2017-11-19 06:10:00";
+        String endTime="2017-11-19 09:59:59";
+        String format="auto";
+        try {
+            Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
+            method.setAccessible(true);
+            method.invoke(per, sourceId,name,startTime,endTime,format);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+
+    }
+
+
+
+
+    @Test
+    public void testDiagramDate_auto_month2() throws Exception {
+//TODO: Test goes here...
+        PerformanceController per = new PerformanceController();
+        String sourceId="1101ZTHX1MMEGJM1W1";
+        String name="IRATHO.SuccOutToUtran";
+        String startTime="2017-02-15 06:10:00";
+        String endTime="2017-11-28 09:59:59";
+        String format="auto";
+        try {
+            Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
+            method.setAccessible(true);
+            method.invoke(per, sourceId,name,startTime,endTime,format);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+
+    }
+
+
+
+
+    @Test
+    public void testDiagramDate_auto_month1() throws Exception {
+//TODO: Test goes here...
+        PerformanceController per = new PerformanceController();
+        String sourceId="1101ZTHX1MMEGJM1W1";
+        String name="MM.AttEpsAttach";
+        String startTime="2017-09-10 06:10:00";
+        String endTime="2017-11-19 07:59:59";
+        String format="auto";
+        try {
+            Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
+            method.setAccessible(true);
+            method.invoke(per, sourceId,name,startTime,endTime,format);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+
+    }
+
+
+
+    @Test
+    public void testDiagramDate_auto_day1() throws Exception {
+//TODO: Test goes here...
+        PerformanceController per = new PerformanceController();
+        String sourceId="1101ZTHX1MMEGJM1W1";
+        String name="IRATHO.SuccIncFromUtran";
+        String startTime="2017-10-17 06:10:00";
+        String endTime="2017-10-19 07:59:59";
+        String format="auto";
+        try {
+            Method method = per.getClass().getDeclaredMethod("diagramDate", String.class, String.class, String.class, String.class, String.class);
+            method.setAccessible(true);
+            method.invoke(per, sourceId,name,startTime,endTime,format);
+        } catch(NoSuchMethodException e) {
+        } catch(IllegalAccessException e) {
+        } catch(InvocationTargetException e) {
+        }
+
+    }
 
 } 
