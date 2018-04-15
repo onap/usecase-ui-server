7df925963f8592cc0547af9f326e92f15d3c5cb7
zhangab <zhanganbing@chinamobile.com>
2018-04-15 09:28:58 +0800

    improve sonar coverage for uui-server
    Change-Id: I397ba32c949099d27eb9d1a53bdc429289bbed32
    Issue-ID: USECASEUI-103
    Signed-off-by: zhangab <zhanganbing@chinamobile.com>

---
+++ server/src/test/java/org/onap/usecaseui/server/service/impl/PerformanceHeaderServiceImplTest.java
@@ -0,0 +1,374 @@
+/*
+ * Copyright (C) 2017 CMCC, Inc. and others. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.usecaseui.server.service.impl;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.onap.usecaseui.server.UsecaseuiServerApplication;
+import org.onap.usecaseui.server.bean.PerformanceHeader;
+import org.onap.usecaseui.server.service.impl.PerformanceHeaderServiceImpl;
+import org.onap.usecaseui.server.util.DateUtils;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.annotation.Resource;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.*;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import mockit.Mock;
+import mockit.MockUp;
+import static org.mockito.Mockito.mock;
+
+/** 
+* PerformanceHeaderServiceImpl Tester. 
+* 
+* @author <Authors name> 
+* @since <pre> 8, 2018</pre>
+* @version 1.0 
+*/
+public class PerformanceHeaderServiceImplTest {
+
+	PerformanceHeaderServiceImpl performanceHeaderServiceImpl = null;
+	private static final long serialVersionUID = 1L;
+
+	@Before
+	public void before() throws Exception {
+		performanceHeaderServiceImpl = new PerformanceHeaderServiceImpl();
+
+		MockUp<Transaction> mockUpTransaction = new MockUp<Transaction>() {
+			@Mock
+			public void commit() {
+			}
+		};
+		MockUp<Query> mockUpQuery = new MockUp<Query>() {
+		};
+		new MockUp<Query>() {
+			@Mock
+			public Query setString(String name, String value) {
+				return mockUpQuery.getMockInstance();
+			}
+			@Mock
+			public Query setDate(String name, Date value) {
+				return mockUpQuery.getMockInstance();
+			}
+			@Mock
+			public Query setInteger(String name, int value) {
+				return mockUpQuery.getMockInstance();
+			}
+			@Mock
+			public int executeUpdate() {
+				return 0;
+			}
+			@Mock
+			public Query setMaxResults(int value) {
+				return mockUpQuery.getMockInstance();
+			}
+			@Mock
+			public Query setFirstResult(int firstResult) {
+				return mockUpQuery.getMockInstance();
+			}
+			@Mock
+			public Query setParameterList(String name, Object[] values) {
+				return mockUpQuery.getMockInstance();
+			}
+			@Mock
+			public List<PerformanceHeader> list() {
+				PerformanceHeader ph = new PerformanceHeader();
+				return Arrays.asList(ph);
+			}
+			@Mock
+			public Object uniqueResult() {
+				return "0";
+			}
+		};
+		MockUp<Session> mockedSession = new MockUp<Session>() {
+			@Mock
+			public Query createQuery(String sql) {
+				return mockUpQuery.getMockInstance();
+			}
+			@Mock
+			public Transaction beginTransaction() {
+				return mockUpTransaction.getMockInstance();
+			}
+			@Mock
+			public Transaction getTransaction() {
+				return mockUpTransaction.getMockInstance();
+			}
+			@Mock
+			public Serializable save(Object object) {
+				return (Serializable) serialVersionUID;
+			}
+			@Mock
+			public void flush() {
+			}
+			@Mock
+			public void update(Object object) {
+			}
+		};
+		new MockUp<SessionFactory>() {
+			@Mock
+			public Session openSession() {
+				return mockedSession.getMockInstance();
+			}
+		};
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() {
+				return mockedSession.getMockInstance();
+			}
+		};
+	}
+
+	@After
+	public void after() throws Exception {
+	}
+
+	@Test
+	public void testSavePerformanceHeader() throws Exception {
+		PerformanceHeader ph = null;
+		performanceHeaderServiceImpl.savePerformanceHeader(ph);
+	}
+
+	@Test
+	public void testUpdatePerformanceHeader() throws Exception {
+		PerformanceHeader ph = null;
+		performanceHeaderServiceImpl.updatePerformanceHeader(ph);
+	}
+
+	@Test
+	public void testGetAllCountByEventType() throws Exception {
+		performanceHeaderServiceImpl.getAllCountByEventType();
+	}
+
+	@Test
+	public void testGetAllByEventType() throws Exception {
+		new MockUp<Query>() {
+			@Mock
+			public List<PerformanceHeader> list() {
+				PerformanceHeader ph = new PerformanceHeader();
+				return Arrays.asList(ph);
+			}
+		};
+		performanceHeaderServiceImpl.getAllByEventType("eventName", "sourceName", "reportingEntityName", DateUtils.now(), DateUtils.now());
+	}
+
+	@Test
+	public void testGetPerformanceHeaderDetail() throws Exception {
+		performanceHeaderServiceImpl.getPerformanceHeaderDetail(1);
+	}
+
+	@Test
+	public void testGetAllByDatetime() throws Exception {
+		performanceHeaderServiceImpl.getAllByDatetime("eventId", "createTime");
+	}
+
+	@Test
+	public void testGetAllCount() throws Exception {
+		PerformanceHeader ph = new PerformanceHeader();
+		ph.setVersion("version");
+		ph.setEventName("eventName");
+		ph.setDomain("domain");
+		ph.setEventId("eventId");
+		ph.setNfcNamingCode("nfcNamingCode");
+		ph.setNfNamingCode("nfNamingCode");
+		ph.setSourceId("sourceId");
+		ph.setSourceName("sourceName");
+		ph.setReportingEntityId("reportingEntityId");
+		ph.setReportingEntityName("reportingEntityName");
+		ph.setPriority("priority");
+		ph.setStartEpochMicrosec("startEpochMicrosec");
+		ph.setLastEpochMicroSec("lastEpochMicroSec");
+		ph.setSequence("sequence");
+		ph.setMeasurementsForVfScalingVersion("measurementsForVfScalingVersion");
+		ph.setMeasurementInterval("measurementInterval");
+		ph.setEventType("eventType");
+		ph.setCreateTime(DateUtils.now());
+		ph.setUpdateTime(DateUtils.now());
+		performanceHeaderServiceImpl.getAllCount(ph, 1, 10);
+	}
+
+	@Test
+	public void testQueryPerformanceHeader() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private int getAllCount(PerformanceHeader performanceHeader, int currentPage, int pageSize) {
+				return 10;
+			}
+		};
+		PerformanceHeader ph = new PerformanceHeader();
+		ph.setVersion("version");
+		ph.setEventName("eventName");
+		ph.setDomain("domain");
+		ph.setEventId("eventId");
+		ph.setNfcNamingCode("nfcNamingCode");
+		ph.setNfNamingCode("nfNamingCode");
+		ph.setSourceId("sourceId");
+		ph.setSourceName("sourceName");
+		ph.setReportingEntityId("reportingEntityId");
+		ph.setReportingEntityName("reportingEntityName");
+		ph.setPriority("priority");
+		ph.setStartEpochMicrosec("startEpochMicrosec");
+		ph.setLastEpochMicroSec("lastEpochMicroSec");
+		ph.setSequence("sequence");
+		ph.setMeasurementsForVfScalingVersion("measurementsForVfScalingVersion");
+		ph.setMeasurementInterval("measurementInterval");
+		ph.setEventType("eventType");
+		ph.setCreateTime(DateUtils.now());
+		ph.setUpdateTime(DateUtils.now());
+		performanceHeaderServiceImpl.queryPerformanceHeader(ph, 1, 10);
+	}
+
+	@Test
+	public void testQueryId() throws Exception {
+		String[] id = {};
+		performanceHeaderServiceImpl.queryId(id);
+	}
+
+	@Test
+	public void testQueryAllSourceId() throws Exception {
+		String[] id = {};
+		performanceHeaderServiceImpl.queryAllSourceId();
+	}
+
+	@Test(expected = Exception.class)
+	public void testSavePerformanceHeaderException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		PerformanceHeader ph = null;
+		performanceHeaderServiceImpl.savePerformanceHeader(ph);
+	}
+
+	@Test(expected = Exception.class)
+	public void testUpdatePerformanceHeaderException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		PerformanceHeader ph = null;
+		performanceHeaderServiceImpl.updatePerformanceHeader(ph);
+	}
+
+	@Test(expected = Exception.class)
+	public void testGetAllCountByEventTypeException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		performanceHeaderServiceImpl.getAllCountByEventType();
+	}
+
+	@Test(expected = Exception.class)
+	public void testGetAllByEventTypeException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		performanceHeaderServiceImpl.getAllByEventType("eventName", "sourceName", "reportingEntityName", DateUtils.now(), DateUtils.now());
+	}
+
+	@Test(expected = Exception.class)
+	public void testGetPerformanceHeaderDetailException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		performanceHeaderServiceImpl.getPerformanceHeaderDetail(1);
+	}
+
+	@Test(expected = Exception.class)
+	public void testGetAllByDatetimeException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		performanceHeaderServiceImpl.getAllByDatetime("eventId", "createTime");
+	}
+
+	@Test(expected = Exception.class)
+	public void testGetAllCountException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		PerformanceHeader ph = new PerformanceHeader();
+		performanceHeaderServiceImpl.getAllCount(ph, 1, 10);
+	}
+
+	@Test(expected = Exception.class)
+	public void testQueryPerformanceHeaderException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		PerformanceHeader ph = new PerformanceHeader();
+		performanceHeaderServiceImpl.queryPerformanceHeader(ph, 1, 10);
+	}
+
+	@Test(expected = Exception.class)
+	public void testQueryIdException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		String[] id = {};
+		performanceHeaderServiceImpl.queryId(id);
+	}
+
+	@Test(expected = Exception.class)
+	public void testQueryAllSourceIdException() throws Exception {
+		new MockUp<PerformanceHeaderServiceImpl>() {
+			@Mock
+			private Session getSession() throws Exception {
+				throw new Exception();
+			}
+		};
+		String[] id = {};
+		performanceHeaderServiceImpl.queryAllSourceId();
+	}
+}
---
+++ server/src/test/java/org/onap/usecaseui/server/service/impl/PerformanceInformationServiceImplTest.java
@@ -0,0 +1,241 @@
+/*
+ * Copyright (C) 2017 CMCC, Inc. and others. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.usecaseui.server.service.impl;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.onap.usecaseui.server.UsecaseuiServerApplication;
+import org.onap.usecaseui.server.bean.PerformanceInformation;
+import org.onap.usecaseui.server.service.PerformanceInformationService;
+import org.onap.usecaseui.server.util.DateUtils;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.annotation.Resource;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import mockit.Mock;
+import mockit.MockUp;
+
+import static org.mockito.Mockito.mock;
+
+/** 
+* PerformanceInformationServiceImpl Tester. 
+* 
+* @author <Authors name> 
+* @since <pre> 8, 2018</pre>
+* @version 1.0 
+*/
+public class PerformanceInformationServiceImplTest {
+    /*@Resource(name = "PerformanceInformationService")
+    PerformanceInformationService performanceInformationService;*/
+    PerformanceInformationServiceImpl service;
+@Before
+public void before() throws Exception {
+    service = mock(PerformanceInformationServiceImpl.class);
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+private Session session;
+private Transaction transaction;
+private Query query;
+/**
+ * mockupUtil 
+ */
+public void mockupUtil(){
+	 MockUp<Query> mockUpQuery = new MockUp<Query>() {
+     };
+		MockUp<Session> mockedSession = new MockUp<Session>() {
+         @Mock
+         public Query createQuery(String sql) {
+             return mockUpQuery.getMockInstance();
+         }
+			@Mock
+			public Transaction beginTransaction() {
+				return transaction;
+			}
+		};
+		new MockUp<SessionFactory>() {
+			@Mock
+			public Session openSession() {
+				return mockedSession.getMockInstance();
+			}
+		};
+		new MockUp<Transaction>() {
+			@Mock
+			public void commit() {
+			}
+		};
+     new MockUp<AlarmsInformationServiceImpl>() {
+         @Mock
+         private Session getSession() {
+             return mockedSession.getMockInstance();
+         }
+     };
+}
+/** 
+* 
+* Method: savePerformanceInformation(PerformanceInformation performanceInformation) 
+* 
+*/ 
+@Test
+public void testSavePerformanceInformation() throws Exception { 
+//TODO: Test goes here...
+
+    PerformanceInformation a = new PerformanceInformation();
+
+    a.setEventId("123");
+    a.setName("SGS.UeUnreachable");
+    a.setValue("40");
+    a.setCreateTime(DateUtils.now());
+    a.setUpdateTime(DateUtils.now());
+    mockupUtil();
+    service.savePerformanceInformation(a);
+
+} 
+
+/** 
+* 
+* Method: updatePerformanceInformation(PerformanceInformation performanceInformation) 
+* 
+*/ 
+@Test
+public void testUpdatePerformanceInformation() throws Exception { 
+//TODO: Test goes here...
+
+    PerformanceInformation a = new PerformanceInformation();
+    a.setEventId("110");
+    a.setName("efw");
+    a.setValue("fko11");
+    a.setUpdateTime(DateUtils.now());
+    a.setCreateTime(DateUtils.now());
+    mockupUtil();
+    service.updatePerformanceInformation(a);
+} 
+
+/** 
+* 
+* Method: getAllCount(PerformanceInformation performanceInformation, int currentPage, int pageSize) 
+* 
+*/ 
+@Test
+public void testGetAllCount() throws Exception { 
+//TODO: Test goes here...
+
+    PerformanceInformation performanceInformation = new PerformanceInformation();
+    performanceInformation.setName("vnf_a_3");
+
+    mockupUtil();
+    service.getAllCount(performanceInformation,0,12);
+
+} 
+
+/** 
+* 
+* Method: queryPerformanceInformation(PerformanceInformation performanceInformation, int currentPage, int pageSize) 
+* 
+*/ 
+@Test
+public void testQueryPerformanceInformation() throws Exception { 
+//TODO: Test goes here...
+    PerformanceInformation a = new PerformanceInformation();
+    // a.setEventId("2202");
+    mockupUtil();
+    service.queryPerformanceInformation(a, 1, 100);
+           // .getList().forEach(al -> System.out.println(al.getValue()));
+} 
+
+/** 
+* 
+* Method: queryId(String[] id) 
+* 
+*/ 
+@Test
+public void testQueryId() throws Exception { 
+//TODO: Test goes here...
+	mockupUtil();
+    service.queryId(new String[]{"2202"});
+          //  .forEach(ai -> System.out.println(ai.getCreateTime()));
+} 
+
+/** 
+* 
+* Method: queryDateBetween(String eventId, Date startDate, Date endDate) 
+* 
+*/ 
+@Test
+public void testQueryDateBetweenForEventIdStartDateEndDate() throws Exception { 
+//TODO: Test goes here...
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    String star = "2017-11-15 06:30:00";
+    String end="2017-11-15 14:45:10";
+    Date stard = sdf.parse(star);
+    Date endd = sdf.parse(end);
+   // mockupUtil();
+    service.queryDateBetween("1101ZTHX1MMEGJM1W1",stard,endd);
+} 
+
+/** 
+* 
+* Method: queryDateBetween(String resourceId, String name, String startTime, String endTime) 
+* 
+*/ 
+@Test
+public void testQueryDateBetweenForResourceIdNameStartTimeEndTime() throws Exception { 
+//TODO: Test goes here...
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    String star = "2017-11-15 06:30:00";
+    String end="2017-11-15 14:45:10";
+    Date stard = sdf.parse(star);
+    Date endd = sdf.parse(end);
+    mockupUtil();
+    service.queryDateBetween("1101ZTHX1MMEGJM1W1",stard,endd);
+
+} 
+
+/** 
+* 
+* Method: queryMaxValueByBetweenDate(String sourceId, String name, String startTime, String endTime) 
+* 
+*/ 
+@Test
+public void testQueryMaxValueByBetweenDate() throws Exception { 
+//TODO: Test goes here...
+	mockupUtil();
+    service.queryDateBetween("2202", DateUtils.stringToDate("2017-10-15 01:00:00"), DateUtils.stringToDate("2017-10-15 02:00:00")).forEach(p -> System.out.println(p));
+
+} 
+
+
+} 
