95335bbf654809750a380bec16696ff4b3929e56
zhangab <zhanganbing@chinamobile.com>
2018-04-16 17:40:50 +0800

    improve sonar coverage for uui-server
    Change-Id: Ic79f6e5f3aeebae1ca741ee4f34573146d457f91
    Issue-ID: USECASEUI-103
    Signed-off-by: zhangab <zhanganbing@chinamobile.com>

---
+++ server/src/test/java/org/onap/usecaseui/server/service/lcm/domain/aai/bean/VimInfoTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2017 CMCC, Inc. and others. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.usecaseui.server.service.lcm.domain.aai.bean;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.onap.usecaseui.server.UsecaseuiServerApplication;
+import org.onap.usecaseui.server.service.lcm.domain.aai.bean.*;
+import org.onap.usecaseui.server.util.DateUtils;
+import org.onap.usecaseui.server.util.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.*;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import mockit.Mock;
+import mockit.MockUp;
+
+public class VimInfoTest {
+
+	@Before
+	public void before() throws Exception {
+	}
+
+	@After
+	public void after() throws Exception {
+	}
+
+	@Test
+	public void testVimInfo() throws Exception {
+		VimInfo vi = new VimInfo("cloudOwner", "cloudRegionId");
+		vi.getCloudOwner();
+		vi.getCloudRegionId();
+		vi.hashCode();
+		vi.equals(vi);
+	}
+}
