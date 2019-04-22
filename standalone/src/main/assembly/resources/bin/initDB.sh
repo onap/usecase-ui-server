bbc003bbf57c5d3b639d4a0daef9353d10bfbb54
guochuyicmri <guochuyi@chinamobile.com>
2019-04-22 18:16:35 +0800

    Upgrade uui-server database script
    Change-Id: I06bf773797d8cdcb83eb7127f347d4df9004de21
    Issue-ID: USECASEUI-236
    Signed-off-by: guochuyicmri <guochuyi@chinamobile.com>

--- standalone/src/main/assembly/resources/bin/initDB.sh
+++ standalone/src/main/assembly/resources/bin/initDB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2017 CMCC, Inc. and others. All rights reserved.
+# Copyright (C) 2019 CMCC, Inc. and others. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
