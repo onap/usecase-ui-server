e2f3f07c853f06e7790f39598f8e3431848d5cce
hekeguang <hekeguang@chinamobile.com>
2020-02-18 09:30:43 +0800

    * Slice task management requirements development
    Issue-ID: USECASEUI-368
    Change-Id: I627f4abcecf01b5ef1ff68128fe2136661b4530e
    Signed-off-by: hekeguang <hekeguang@chinamobile.com>

--- server/src/main/java/org/onap/usecaseui/server/util/RestfulServices.java
+++ server/src/main/java/org/onap/usecaseui/server/util/RestfulServices.java
@@ -18,11 +18,27 @@ package org.onap.usecaseui.server.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
 
+import javax.naming.Context;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import org.glassfish.jersey.client.ClientConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,28 +52,33 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class RestfulServices {
 
-	private static final Logger logger = LoggerFactory.getLogger(RestfulServices.class);
+    private static final Logger logger = LoggerFactory.getLogger(RestfulServices.class);
 
     public static <T> T create(String baseUrl, Class<T> clazz) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
+            .baseUrl(baseUrl)
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build();
         return retrofit.create(clazz);
     }
 
     public static <T> T create(Class<T> clazz) {
         //Set the interface response time
-    	final OkHttpClient client = new OkHttpClient.Builder().
-    	        connectTimeout(300, TimeUnit.SECONDS).
-    	        readTimeout(300, TimeUnit.SECONDS).
-    	        writeTimeout(300, TimeUnit.SECONDS).build();
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(300, TimeUnit.SECONDS)
+            .readTimeout(300, TimeUnit.SECONDS)
+            .sslSocketFactory(getSSLSocketFactory(), new CustomTrustManager())
+            .hostnameVerifier(getHostnameVerifier())
+            .build();
+
         String msbUrl = getMsbAddress();
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://" + msbUrl + "/")
-        		.client(client)
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
+            .baseUrl("https://" + msbUrl + "/")
+            .client(okHttpClient)
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build();
+
         return retrofit.create(clazz);
     }
 
@@ -70,25 +91,47 @@ public class RestfulServices {
     }
 
     public static RequestBody extractBody(HttpServletRequest request) throws IOException {
-      	 BufferedReader br = null;
-       	 StringBuilder sb = new StringBuilder("");
-       	 try {
-       		 br = request.getReader();
-                String str;
-                while ((str = br.readLine()) != null)
-                {
-                    sb.append(str);
-                }
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder("");
+        try {
+            br = request.getReader();
+            String str;
+            while ((str = br.readLine()) != null) {
+                sb.append(str);
+            }
+            br.close();
+            logger.info("The request body content is: " + sb.toString());
+            return RequestBody.create(MediaType.parse("application/json"), sb.toString());
+        } catch (Exception e) {
+            logger.info("RestfulServices occur exection,this content is: " + e.getMessage());
+            return RequestBody.create(MediaType.parse("application/json"), sb.toString());
+        } finally {
+            if (null != br) {
                 br.close();
-                logger.info("The request body content is: "+sb.toString());
-                return RequestBody.create(MediaType.parse("application/json"),sb.toString());
-    		}catch(Exception e){
-    			 logger.info("RestfulServices occur exection,this content is: "+e.getMessage());
-    			 return RequestBody.create(MediaType.parse("application/json"),sb.toString());
-    		}finally {
-               if (null != br) {
-               	 br.close();
-               }
-    		}
+            }
         }
+    }
+
+    public static SSLSocketFactory getSSLSocketFactory() {
+        SSLSocketFactory ssfFactory = null;
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, new TrustManager[]{new CustomTrustManager()}, new SecureRandom());
+
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception e) {
+        }
+
+        return ssfFactory;
+    }
+
+    public static HostnameVerifier getHostnameVerifier() {
+        HostnameVerifier   hostnameVerifier= new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        return hostnameVerifier;
+    }
 }
