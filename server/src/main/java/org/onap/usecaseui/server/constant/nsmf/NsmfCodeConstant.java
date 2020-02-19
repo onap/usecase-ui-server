bf4c6135581cf6491dac7680148885f6d1e5cfaf
hekeguang <hekeguang@chinamobile.com>
2020-02-19 10:25:47 +0800

    Add nsmf related constant definition
    Issue-ID: USECASEUI-374
    Change-Id: I659c6e626cb6631da3802e5ba2cf58a09c36b2c2
    Signed-off-by: hekeguang <hekeguang@chinamobile.com>

---
+++ server/src/main/java/org/onap/usecaseui/server/constant/nsmf/NsmfCodeConstant.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2019 CMCC, Inc. and others. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.usecaseui.server.constant.nsmf;
+
+public class NsmfCodeConstant {
+    public static final String SUCCESS_CODE = "200";
+
+    public static final String ERROR_CODE_UNKNOWN = "500";
+
+    public static final int RESOURCE_NOT_FOUND_CODE = 404;
+
+    public static final String ACTIVATE_TYPE = "activate";
+
+    public static final String DEACTIVATE_TYPE = "deactivate";
+
+    public static final String DELETE_TYPE = "delete";
+
+    public static final String OPERATION_ERROR_STATUS = "error";
+}
---
+++ server/src/main/java/org/onap/usecaseui/server/constant/nsmf/NsmfParamConstant.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2019 CMCC, Inc. and others. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.usecaseui.server.constant.nsmf;
+
+public class NsmfParamConstant {
+    public static final String CUSTOM_5G = "5GCustomer";
+
+    public static final String SERVICE_TYPE_5G = "5G";
+
+    public static final String SERVICE_INSTANCE = "service-instance";
+
+    public static final String ALLOTTED_RESOURCE = "allotted-resource";
+
+    public static final String  SERVICE_INSTANCE_SERVICE_INSTANCE_ID = "service-instance.service-instance-id";
+
+    public static final String AN_NAME = "an";
+
+    public static final String TN_NAME = "tn";
+
+    public static final String CN_NAME = "cn";
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String SPACE = " ";
+}
