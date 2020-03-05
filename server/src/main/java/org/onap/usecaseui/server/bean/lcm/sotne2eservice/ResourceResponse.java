adee87e7191ffba35a273166ba1dfb819fe66941
seshukm <seshu.kumar.m@huawei.com>
2020-03-05 07:44:14 +0530

    Issue with the service loading and correct the license info
    Issue-ID: USECASEUI-387
    
    Signed-off-by: seshukm <seshu.kumar.m@huawei.com>
    Change-Id: If4ed5a39941cb25ead173e92c145287fb280d477

--- server/src/main/java/org/onap/usecaseui/server/bean/lcm/sotne2eservice/ResourceResponse.java
+++ server/src/main/java/org/onap/usecaseui/server/bean/lcm/sotne2eservice/ResourceResponse.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016-2017 ZTE Corporation.
+ * Copyright 2020 Huawei Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
