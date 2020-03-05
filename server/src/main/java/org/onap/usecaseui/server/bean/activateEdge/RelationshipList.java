ca1e4abe96323a4451acf2208ce60d444e623d53
seshukm <seshu.kumar.m@huawei.com>
2020-03-05 06:22:24 +0530

    Fix the lcm object reference
    Issue-ID: USECASEUI-387
    
    Signed-off-by: seshukm <seshu.kumar.m@huawei.com>
    Change-Id: I19c15844b03ab6c68a3dadba79c41c9ffec21ced

--- server/src/main/java/org/onap/usecaseui/server/bean/activateEdge/RelationshipList.java
+++ server/src/main/java/org/onap/usecaseui/server/bean/activateEdge/RelationshipList.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2017 CMCC, Inc. and others. All rights reserved.
+/**
+ * Copyright (C) 2020 Huawei, Inc. and others. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 package org.onap.usecaseui.server.bean.activateEdge;
 
 import com.fasterxml.jackson.annotation.*;
--- server/src/main/java/org/onap/usecaseui/server/controller/lcm/SotnServiceLcmController.java
+++ server/src/main/java/org/onap/usecaseui/server/controller/lcm/SotnServiceLcmController.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2016-2017 ZTE Corporation.
+/*
+ * Copyright (C) 2020 Huawei, Inc. and others. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
