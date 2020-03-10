5320457860b81d6e655cf5877fc0873f71dbfa82
mukesh.paliwal1@huawei.com <mukesh.paliwal1@huawei.com>
2020-03-10 09:52:09 +0530

    UT for SOTN Eline in USECASEUI
    Issue-ID: USECASEUI-378
    
    Signed-off-by: mukesh.paliwal1@huawei.com <mukesh.paliwal1@huawei.com>
    Change-Id: Icd0ddd9aac2c1a53d4fde71ed81172e83fae6b97

---
+++ server/src/test/java/org/onap/usecaseui/server/bean/lcm/sotne2eservice/E2EServiceInstanceRequestTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2020 Huawei, Inc. and others. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.usecaseui.server.bean.lcm.sotne2eservice;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+
+public class E2EServiceInstanceRequestTest {
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+    @Test
+    public void  testGetE2EServiceInstanceRequest() throws Exception{
+        E2EServiceInstanceRequest e2esir = new E2EServiceInstanceRequest();
+        e2esir.getAdditionalProperties();
+        e2esir.getService();
+
+    }
+    @Test
+    public void  testSetE2EServiceInstanceRequest() throws Exception{
+        E2EServiceInstanceRequest e2esir = new E2EServiceInstanceRequest();
+        e2esir.setAdditionalProperty("",anyObject());
+        e2esir.setAdditionalProperties(null);
+        e2esir.setService(any());
+    }
+}
