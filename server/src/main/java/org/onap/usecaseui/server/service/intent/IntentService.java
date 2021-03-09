10964526a0738af422224ed1fbaf9cd119efaca6
zhaoyehua <zhaoyh6@asiainfo.com>
2021-03-09 15:37:51 +0800

    feat:Implementation and docking of natural language parsing function of UUI server.
    Issue-ID: USECASEUI-525
    Change-Id: I95ce7e3dfe0071ac0f14d74bbf0f140caa8e904a
    Signed-off-by: zhaoyehua <zhaoyh6@asiainfo.com>

---
+++ server/src/main/java/org/onap/usecaseui/server/service/intent/IntentService.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017 CTC, Inc. and others. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.usecaseui.server.service.intent;
+
+import org.onap.usecaseui.server.bean.intent.IntentModel;
+
+import java.util.List;
+
+public interface IntentService {
+    public String addModel(IntentModel model);
+    List<IntentModel> listModels();
+    public String deleteModel(String modelId);
+    public IntentModel getModel(String modelId);
+    public IntentModel activeModel(String modelId);
+    String activeModelFile(IntentModel model);
+}
