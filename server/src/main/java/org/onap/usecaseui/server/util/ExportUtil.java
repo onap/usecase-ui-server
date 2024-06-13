80e515976435409b73ba0971a5abb424be256f6f
zhen1234 <liz779@chinatelecom.cn>
2024-06-13 11:01:15 +0800

    add exportUtil
    Issue-ID: USECASEUI-837
    Signed-off-by: zhen1234 <liz779@chinatelecom.cn>
    Change-Id: I760436c6a83342258c33ecf1329bb203e3857b72

---
+++ server/src/main/java/org/onap/usecaseui/server/util/ExportUtil.java
@@ -0,0 +1,51 @@
+package org.onap.usecaseui.server.util;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@SuppressWarnings("unused")
+public class ExportUtil {
+
+    public static void exportExcel(HttpServletResponse response, String fileName, List<?> dataList) {
+        SXSSFWorkbook book = new SXSSFWorkbook();
+        Sheet sheet = book.createSheet();
+        CellStyle rowStyle = book.createCellStyle();
+        rowStyle.setAlignment(HorizontalAlignment.CENTER);
+        rowStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        sheet.setDefaultColumnWidth(15);
+        for (int i = 0; i < dataList.size(); i++) {
+            Row row = sheet.createRow(i);
+            Cell cell = row.createCell(0);
+            cell.setCellStyle(rowStyle);
+            cell.setCellType(CellType.STRING);
+            cell.setCellValue((String) dataList.get(i));
+        }
+        try {
+            write(response, book, fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void write(HttpServletResponse response, SXSSFWorkbook book, String fileName) throws IOException {
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setCharacterEncoding("utf-8");
+        String name = new String(fileName.getBytes("GBK"), "ISO8859_1") + ".xlsx";
+        response.addHeader("Content-Disposition", "attachment;filename=" + name);
+        ServletOutputStream out = response.getOutputStream();
+        book.write(out);
+        out.flush();
+        out.close();
+    }
+}
